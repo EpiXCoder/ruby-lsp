6a182b0de5118dce9f8b1b9d4fe2f3fbd9d77b71
Andy Waite <13400+andyw8@users.noreply.github.com>
2025-02-27 15:30:01 -0500

    Use RPC structure

--- lib/ruby_lsp/test_reporter.rb
+++ lib/ruby_lsp/test_reporter.rb
@@ -9,61 +9,56 @@ module RubyLsp
       #: (id: String, uri: URI::Generic) -> void
       def start_test(id:, uri:)
         result = {
-          event: "start",
           id: id,
           uri: uri.to_s,
         }
-        send_message(result)
+        send_message("start", result)
       end
 
       #: (id: String, uri: URI::Generic) -> void
       def record_pass(id:, uri:)
         result = {
-          event: "pass",
           id: id,
           uri: uri.to_s,
         }
-        send_message(result)
+        send_message("pass", result)
       end
 
       #: (id: String, type: untyped, message: String, uri: URI::Generic) -> void
       def record_fail(id:, type:, message:, uri:)
         result = {
-          event: "fail",
           id: id,
           type: type,
           message: message,
           uri: uri.to_s,
         }
-        send_message(result)
+        send_message("fail", result)
       end
 
       #: (id: String, message: String?, uri: URI::Generic) -> void
       def record_skip(id:, message:, uri:)
         result = {
-          event: "skip",
           id: id,
           message: message,
           uri: uri.to_s,
         }
-        send_message(result)
+        send_message("skip", result)
       end
 
       #: (id: String, message: String?, uri: String) -> void
       def record_error(id:, message:, uri:)
         result = {
-          event: "error",
           id: id,
           message: message,
           uri: uri.to_s,
         }
-        send_message(result)
+        send_message("error", result)
       end
 
       private
 
-      def send_message(result)
-        json_message = result.to_json
+      def send_message(method_name, params)
+        json_message = { method: method_name, params: params }.to_json
         $stdout.write("Content-Length: #{json_message.bytesize}\r\n\r\n#{json_message}")
       end
     end
--- test/test_unit_test_runner_test.rb
+++ test/test_unit_test_runner_test.rb
@@ -21,48 +21,64 @@ module RubyLsp
 
       expected = [
         {
-          "event" => "start",
-          "id" => "Sample#test_that_fails",
-          "uri" => "file://#{project_path}/test/fixtures/test_unit_example.rb",
+          "method" => "start",
+          "params" => {
+            "id" => "Sample#test_that_fails",
+            "uri" => "file://#{project_path}/test/fixtures/test_unit_example.rb",
+          },
         },
         {
-          "event" => "fail",
-          "id" => "Sample#test_that_fails",
-          "type" => "Test::Unit::Failure",
-          "message" => "<1> expected but was\n<2>.",
-          "uri" => "file://#{project_path}/test/fixtures/test_unit_example.rb",
+          "method" => "fail",
+          "params" => {
+            "id" => "Sample#test_that_fails",
+            "type" => "Test::Unit::Failure",
+            "message" => "<1> expected but was\n<2>.",
+            "uri" => "file://#{project_path}/test/fixtures/test_unit_example.rb",
+          },
         },
         {
-          "event" => "start",
-          "id" => "Sample#test_that_is_pending",
-          "uri" => "file://#{project_path}/test/fixtures/test_unit_example.rb",
+          "method" => "start",
+          "params" => {
+            "id" => "Sample#test_that_is_pending",
+            "uri" => "file://#{project_path}/test/fixtures/test_unit_example.rb",
+          },
         },
         {
-          "event" => "skip",
-          "id" => "Sample#test_that_is_pending",
-          "message" => "pending test",
-          "uri" => "file://#{project_path}/test/fixtures/test_unit_example.rb",
+          "method" => "skip",
+          "params" => {
+            "id" => "Sample#test_that_is_pending",
+            "message" => "pending test",
+            "uri" => "file://#{project_path}/test/fixtures/test_unit_example.rb",
+          },
         },
         {
-          "event" => "start",
-          "id" => "Sample#test_that_passes",
-          "uri" => "file://#{project_path}/test/fixtures/test_unit_example.rb",
+          "method" => "start",
+          "params" => {
+            "id" => "Sample#test_that_passes",
+            "uri" => "file://#{project_path}/test/fixtures/test_unit_example.rb",
+          },
         },
         {
-          "event" => "pass",
-          "id" => "Sample#test_that_passes",
-          "uri" => "file://#{project_path}/test/fixtures/test_unit_example.rb",
+          "method" => "pass",
+          "params" => {
+            "id" => "Sample#test_that_passes",
+            "uri" => "file://#{project_path}/test/fixtures/test_unit_example.rb",
+          },
         },
         {
-          "event" => "start",
-          "id" => "Sample#test_that_raises",
-          "uri" => "file://#{project_path}/test/fixtures/test_unit_example.rb",
+          "method" => "start",
+          "params" => {
+            "id" => "Sample#test_that_raises",
+            "uri" => "file://#{project_path}/test/fixtures/test_unit_example.rb",
+          },
         },
         {
-          "event" => "error",
-          "id" => "Sample#test_that_raises",
-          "message" => "RuntimeError: oops",
-          "uri" => "file://#{project_path}/test/fixtures/test_unit_example.rb",
+          "method" => "error",
+          "params" => {
+            "id" => "Sample#test_that_raises",
+            "message" => "RuntimeError: oops",
+            "uri" => "file://#{project_path}/test/fixtures/test_unit_example.rb",
+          },
         },
       ]
       assert_equal(expected, actual)
